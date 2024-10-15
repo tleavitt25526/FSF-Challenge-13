2a442577ce33e81069ed137e773589d528a96f27
tleavitt25526 <tleavitt25526@gmail.com>
2024-10-15 10:07:32 -0400

    Sequelize models finished

--- models/index.js
+++ models/index.js
@@ -1,16 +1,33 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "product_tags",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unqie: false,
+  },
+
+  as: "tag_products",
+});
 
 module.exports = {
   Product,
